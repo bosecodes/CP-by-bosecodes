b54a4f67e4df3b96e967f3e8b9a0aad2794a6dc0
Somdev Basu <somdevbasu100@gmail.com>
2021-06-22 12:39:41 +0530

    Update and rename countSubarrGivenXOR.java to countSubarrGivenXOR_TBD_.java

--- 30 days coding challenge/Day 4: Hashing/countSubarrGivenXOR_TBD_.java
+++ 30 days coding challenge/Day 4: Hashing/countSubarrGivenXOR_TBD_.java
@@ -4,7 +4,7 @@ Count the number of subarrays with a given XOR value k
 Naive : Approach, generate all the subarrays using Kadane's algorithm and then calculate the XOR for each 
 And thereby calculate the total number of subarrays with the given XOR : O(N2)
 
-
+https://www.youtube.com/watch?v=lO9R5CaGRPY&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=24
 
 */
 
