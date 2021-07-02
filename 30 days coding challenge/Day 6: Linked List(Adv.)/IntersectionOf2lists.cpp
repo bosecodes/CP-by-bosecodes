63b118863edaa8cb8d50ce74871423278328fc57
Somdev Basu <somdevbasu100@gmail.com>
2021-07-02 11:43:53 +0530

    Update IntersectionOf2lists.cpp

--- 30 days coding challenge/Day 6: Linked List(Adv.)/IntersectionOf2lists.cpp
+++ 30 days coding challenge/Day 6: Linked List(Adv.)/IntersectionOf2lists.cpp
@@ -23,22 +23,47 @@ O(n) + O(m)
 
 using namespace std;
 
-
-class Solution {
-    pubic:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA == NULL || headB == NULL) return NULL;
-        ListNode *a = headA;
-        ListNode *b = headB;
-        // if a and b have different len we will stop the loop after 
-        // second iteration
-        while(a != b) {
-            a = a == NULL? headB : a->next;
-            b = b == NULL? headA : b->next;
-        }
-        return a;
+int length(node *head) {
+    int l = 0;
+    node *temp = head;
+    while(temp != NULL) {
+        temp = temp->next;
+        l++;
     }
-};
+    return l;
+}
+
+int intersection(node *head1, node *head2) {
+    int l1 = length(head1);
+    int l2 = length(head2);
+    int d = 0;
+    node *ptr1;
+    node *ptr2;
+    if(l1 > l2) {
+        d = l1 - l2;
+        ptr1 = head1;
+        ptr2 = head2;
+    }
+    if(l2 > l1) {
+        d = l2 - l1;
+        ptr1 = head2;
+        ptr2 = head1;
+    }
+    while(d--) {
+        ptr1 = ptr1->next;
+        if(ptr1 == NULL) {
+            return -1;
+        }
+    }
+    
+    while(ptr1 != NULL && ptr2 != NULL) {
+        if(ptr1 == ptr2)
+            return ptr1->data;
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+    return -1;
+}
 
 
 
